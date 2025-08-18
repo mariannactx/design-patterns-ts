3d1c702a930e3bd55689c454c38fc4697d3708e7
mariannactx <mariannactx@gmail.com>
2025-08-18 20:12:43 -0300

    configure stryker to avoid timeouts and run faster. improve tests

--- src/behavioral/strategy/Order.ts
+++ src/behavioral/strategy/Order.ts
@@ -1,7 +1,7 @@
 export default abstract class Order {
   public value!: number;
   private shipping!: Shipping;
-  protected sector!: string;
+  public sector!: string;
 
   constructor(value: number, shipping: Shipping) {
     this.value = value;
--- tests/behavioral/strategy.spec.ts
+++ tests/behavioral/strategy.spec.ts
@@ -4,23 +4,36 @@ import ShippingCommon from '../../src/behavioral/strategy/ShippingCommon';
 import ShippingExpress from '../../src/behavioral/strategy/ShippingExpress';
 
 describe('behavioral/strategy', () => {
+  it('generates correctly each kind of orders', () => {
+    const orderEletronics = new OrderEletronics(100, new ShippingCommon());
+    const orderFurniture = new OrderFurniture(100, new ShippingCommon());
+    expect(orderEletronics.value).toBe(100);
+    expect(orderFurniture.value).toBe(100);
+    expect(orderEletronics.sector).toBe('Eletronics');
+    expect(orderFurniture.sector).toBe('Furniture');
+  });
+
   it('calculates common shipping for electronics', () => {
     const orderEletronics = new OrderEletronics(100, new ShippingCommon());
-    expect(orderEletronics.calculateShipping()).toBe(5);
+    const shippingResult = orderEletronics.calculateShipping();
+    expect(shippingResult).toBe(5);
   });
 
   it('calculates express shipping for electronics', () => {
     const orderEletronics = new OrderEletronics(100, new ShippingExpress());
-    expect(orderEletronics.calculateShipping()).toBe(10);
+    const shippingResult = orderEletronics.calculateShipping();
+    expect(shippingResult).toBe(10);
   });
 
   it('calculates common shipping for furniture', () => {
     const orderFurniture = new OrderFurniture(100, new ShippingCommon());
-    expect(orderFurniture.calculateShipping()).toBe(5);
+    const shippingResult = orderFurniture.calculateShipping();
+    expect(shippingResult).toBe(5);
   });
 
   it('calculates express shipping for furniture', () => {
     const orderFurniture = new OrderFurniture(100, new ShippingExpress());
-    expect(orderFurniture.calculateShipping()).toBe(10);
+    const shippingResult = orderFurniture.calculateShipping();
+    expect(shippingResult).toBe(10);
   });
 });
